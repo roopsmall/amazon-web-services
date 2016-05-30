819bdca5cab3a65d0845689b1fd3dedd79e2e574
roopsmall <roopsmall@outlook.com>
2016-05-30 14:35:59 +0100

    IAM readme and update create_user..

--- create_new_user.py
+++ create_new_user.py
@@ -1,9 +1,10 @@
-import boto3
-import credentials
+import boto3, credentials
 
+# get access key id and secret access key
 id, psswd = credentials.id_and_psswd('user')
 
+#start session
 client = boto3.client('iam', aws_access_key_id=id, aws_secret_access_key=psswd)
-response = client.create_user(Path='/Analyst/', UserName='new_user1')
 
-print response
+# create new user, print result
+print client.create_user(Path='/Analyst/', UserName='new_user1')
